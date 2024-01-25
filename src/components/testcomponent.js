6bd5eb5644d6b9b0ac7a9a181a06f56c2b2e549a
SIDDHARTHRAGAVAN <siddharthragavank@gmail.com>
2024-01-25 09:03:11 +0530

    siddharth

---
+++ src/components/testcomponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { ReactDOM } from "react";
+
+function Testcomponent(){
+    return(
+        <div>
+            <h1>This is a Test Component</h1>
+        </div>
+    )
+}
+
+export default Testcomponent;
